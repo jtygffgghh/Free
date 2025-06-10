59c5c26eb34927599a41719b7fdedc29846ed7e3
jtygffgghh <Elizabeth.potuzhna1h0h9v1@outlook.com>
2025-06-10 20:44:58 +0300

    Update 02-install-openvpn.sh

--- .devcontainer/scripts/02-install-openvpn.sh
+++ .devcontainer/scripts/02-install-openvpn.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "[+] Установка OpenVPN и curl внутри контейнера ubuntu_gui..."
+echo "[02] Установка OpenVPN и curl внутри контейнера ubuntu_gui..."
 
 sudo docker exec ubuntu_gui bash -c "apt update && apt install -y openvpn curl"
 
-echo "[+] Установка завершена."
+echo "[02] Установка завершена."
