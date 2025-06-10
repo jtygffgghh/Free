fe03c193cbd725371becf5310cbbc62a9f8bd153
jtygffgghh <Elizabeth.potuzhna1h0h9v1@outlook.com>
2025-06-10 20:36:27 +0300

    Update bootstrap.sh

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -4,8 +4,7 @@ set -e
 echo "[BOOTSTRAP] Запуск 01-setup-docker.sh..."
 bash "$(dirname "$0")/scripts/01-setup-docker.sh"
 
-# В будущем здесь можно добавлять другие скрипты:
-# bash "$(dirname "$0")/scripts/02-install-vpn.sh"
-# bash "$(dirname "$0")/scripts/03-start-miner.sh"
+echo "[BOOTSTRAP] Запуск 02-install-openvpn.sh..."
+bash "$(dirname "$0")/scripts/02-install-openvpn.sh"
 
 echo "[BOOTSTRAP] Все шаги выполнены успешно."
